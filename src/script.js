5f0a1afd0f31e49895bc13330a3890161e89ca70
Coletha <66093941+colethab@users.noreply.github.com>
2020-09-20 00:57:46 -0700

    fixed imagine-icon
    In both js and html

--- src/script.js
+++ src/script.js
@@ -25,6 +25,8 @@ function formatDate(date) {
 }
 
 function displayWeatherCondition(response) {
+  let iconElement = document.querySelector("#icon");
+
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -35,6 +37,11 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "56db3f0b19ad33f6c1202c7a2cde9052";
